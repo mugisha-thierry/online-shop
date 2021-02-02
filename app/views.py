35376cffa7108929ac9da2e3f1f3e2cff6dd5187
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-02 21:47:27 +0200

    Modifeied views

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
-# from .models import  Profile,Neighbourhood,Neighbourhood_infrastructure,Post
+from .models import Profile,OrderItem, Order, Transaction,Product, Category
 from django.contrib.auth import login, authenticate
 from rest_framework import status
 from rest_framework.response import Response
@@ -11,13 +11,26 @@ from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from django.urls import reverse
 from .forms import SignUpForm, UpdateUserProfileForm,CommentForm
-# from .models import  Profile,Neighbourhood,Neighbourhood_infrastructure,Post,Comment
 from .decorators import admin_only,allowed_users
+from django.contrib import messages
+
+import datetime
+# import stripe
 
 # Create your views here.
 # @login_required(login_url='login')
 def home(request):
-    return render(request, 'home.html')
+    object_list = Product.objects.all()
+    categorys = Category.get_category()
+    return render(request, 'home.html',{'object_list':object_list,'categorys':categorys})
+
+def product_category(request, category):
+    object_list = Product.filter_by_category(category)
+    categorys = Category.get_category()
+    context = {'object_list':object_list,'categorys': categorys}
+    return render(request,'category/notlogged.html',context)
+    
+       
 
 def signup(request):
     if request.method == 'POST':
@@ -30,3 +43,203 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'register/register.html', {'form': form}) 
+
+def profile(request, username):
+    my_user_profile = Profile.objects.filter(user=request.user).first()
+    my_orders = Order.objects.filter(is_ordered=True, owner=my_user_profile)
+    if request.method == 'POST':
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if prof_form.is_valid():
+            prof_form.save()
+            return redirect(request.path_info)
+    else:
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+
+    context = {
+        'prof_form': prof_form,
+        'my_orders':my_orders,
+
+    }
+    return render(request, 'profile.html', context)
+
+@login_required(login_url='login')
+def product_list(request):
+    object_list = Product.objects.all()
+    categorys = Category.get_category()
+    filtered_orders = Order.objects.filter(owner=request.user.profile, is_ordered=False)
+    current_order_products = []
+    if filtered_orders.exists():
+    	user_order = filtered_orders[0]
+    	user_order_items = user_order.items.all()
+    	current_order_products = [product.product for product in user_order_items]
+
+    context = {
+        'object_list': object_list,
+        'current_order_products': current_order_products,
+        'categorys':categorys
+    }
+
+    return render(request, "products/product_list.html", context)
+
+def products_category(request, category):
+    object_list = Product.filter_by_category(category)
+    categorys = Category.get_category()
+    filtered_orders = Order.objects.filter(owner=request.user.profile, is_ordered=False)
+    current_order_products = []
+    if filtered_orders.exists():
+    	user_order = filtered_orders[0]
+    	user_order_items = user_order.items.all()
+    	current_order_products = [product.product for product in user_order_items]
+    context = {'object_list':object_list,'categorys': categorys,'current_order_products':current_order_products}
+    return render(request,'category/logedin.html',context)     
+
+def get_user_pending_order(request):
+    # get order for the correct user
+    user_profile = get_object_or_404(Profile, user=request.user)
+    order = Order.objects.filter(owner=user_profile, is_ordered=False)
+    if order.exists():
+        # get the only order in the list of filtered orders
+        return order[0]
+    return 0
+
+
+@login_required()
+def add_to_cart(request, **kwargs):
+    # get the user profile
+    user_profile = get_object_or_404(Profile, user=request.user)
+    # filter products by id
+    product = Product.objects.filter(id=kwargs.get('item_id', "")).first()
+    # check if the user already owns this product
+    # if product in request.user.profile.ebooks.all():
+    #     messages.info(request, 'You already own this ebook')
+    #     return redirect(reverse('product_list')) 
+    # create orderItem of the selected product
+    order_item, status = OrderItem.objects.get_or_create(product=product)
+    # create order associated with the user
+    user_order, status = Order.objects.get_or_create(owner=user_profile, is_ordered=False)
+    user_order.items.add(order_item)
+    if status:
+        # generate a reference code
+        user_order.ref_code = generate_order_id()
+        user_order.save()
+
+    # show confirmation message and redirect back to the same page
+    messages.info(request, "item added to cart")
+    return redirect(reverse('product_list'))
+
+
+@login_required(login_url='login')
+def delete_from_cart(request, item_id):
+    item_to_delete = OrderItem.objects.filter(pk=item_id)
+    if item_to_delete.exists():
+        item_to_delete[0].delete()
+        messages.info(request, "Item has been deleted")
+    return redirect(reverse('order_summary'))
+
+
+@login_required(login_url='login')
+def order_details(request, **kwargs):
+    existing_order = get_user_pending_order(request)
+    context = {
+        'order': existing_order
+    }
+    return render(request, 'shopping_cart/order_summary.html', context)
+
+
+@login_required(login_url='login')
+def checkout(request, **kwargs):
+    client_token = 222
+    existing_order = get_user_pending_order(request)
+    publishKey = 111
+    if request.method == 'POST':
+        token = request.POST.get('stripeToken', False)
+        if token:
+            try:
+                charge = stripe.Charge.create(
+                    amount=100*existing_order.get_cart_total(),
+                    currency='usd',
+                    description='Example charge',
+                    source=token,
+                )
+
+                return redirect(reverse('shopping_cart:update_records',
+                        kwargs={
+                            'token': token
+                        })
+                    )
+            except stripe.CardError as e:
+                message.info(request, "Your card has been declined.")
+        else:
+            result = transact({
+                'amount': existing_order.get_cart_total(),
+                'payment_method_nonce': request.POST['payment_method_nonce'],
+                'options': {
+                    "submit_for_settlement": True
+                }
+            })
+
+            if result.is_success or result.transaction:
+                return redirect(reverse('shopping_cart:update_records',
+                        kwargs={
+                            'token': result.transaction.id
+                        })
+                    )
+            else:
+                for x in result.errors.deep_errors:
+                    messages.info(request, x)
+                return redirect(reverse('shopping_cart:checkout'))
+            
+    context = {
+        'order': existing_order,
+        'client_token': client_token,
+        'STRIPE_PUBLISHABLE_KEY': publishKey
+    }
+
+    return render(request, 'shopping_cart/checkout.html', context)
+
+
+@login_required(login_url='login')
+def update_transaction_records(request, token):
+    # get the order being processed
+    order_to_purchase = get_user_pending_order(request)
+
+    # update the placed order
+    order_to_purchase.is_ordered=True
+    order_to_purchase.date_ordered=datetime.datetime.now()
+    order_to_purchase.save()
+    
+    # get all items in the order - generates a queryset
+    order_items = order_to_purchase.items.all()
+
+    # update order items
+    order_items.update(is_ordered=True, date_ordered=datetime.datetime.now())
+
+    # Add products to user profile
+    user_profile = get_object_or_404(Profile, user=request.user)
+    # get the products from the items
+    order_products = [item.product for item in order_items]
+    user_profile.ebooks.add(*order_products)
+    user_profile.save()
+
+    
+    # create a transaction
+    transaction = Transaction(profile=request.user.profile,
+                            token=token,
+                            order_id=order_to_purchase.id,
+                            amount=order_to_purchase.get_cart_total(),
+                            success=True)
+    # save the transcation (otherwise doesn't exist)
+    transaction.save()
+
+
+    # send an email to the customer
+    # look at tutorial on how to send emails with sendgrid
+    messages.info(request, "Thank you! Your purchase was successful!")
+    return redirect(reverse('accounts:my_profile'))
+
+
+def success(request, **kwargs):
+    # a view signifying the transcation was successful
+    return render(request, 'shopping_cart/purchase_success.html', {})
+
+
