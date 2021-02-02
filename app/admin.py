8429742bb6771f209497435f7a190c241083a33f
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-02 11:41:02 +0200

    Added models to admin page

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Profile, Comment
+from .models import Profile, Comment,Category,Product,OrderItem,Order,Transaction
 
 # Register your models here.
 
-# admin.site.register(Neighbourhood)
+admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Profile)
-# admin.site.register(Neighbourhood_infrastructure)
+admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(Transaction)
+# admin.site.register(OrderItem)
