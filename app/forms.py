a81dec3584b8655e15563d95cd0b2f408a88ec5f
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-04 19:55:31 +0200

    Created forms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile,Comment
+from .models import Profile, Comment,Rate,Delivery
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
@@ -15,12 +15,31 @@ class UpdateUserProfileForm(forms.ModelForm):
         model = Profile
         exclude = ['user']
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        exclude = ['user', 'post']                  
+                
 
 class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['user']        
+        exclude = ['user']
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
+
+        
+class DeliveryForm(forms.ModelForm):
+    class Meta:
+        model = Delivery
+        exclude = ['user']
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        exclude = ['user', 'product']
